09363729f579cf1b6552089dd36358eb127a5057
Michael Goff <michaelgoff@Michaels-MacBook-Pro.local>
2022-07-19 16:19:45 -0700

    Display table names

--- Soccer/get_db.py
+++ Soccer/get_db.py
@@ -9,4 +9,30 @@ cursor = conn.cursor()
 table = pd.read_sql("""SELECT *
                         FROM sqlite_master
                         WHERE type='table';""", conn)
-print(table)
+table_names = table["tbl_name"].to_list()[1:]
+
+tables = {}
+column_names = {}
+column_lengths = {}
+for i in range(len(table_names)):
+    query = "SELECT * FROM {0} LIMIT 1".format(table_names[i])
+    tables[table_names[i]] = pd.read_sql(query, conn)
+    column_names[table_names[i]] = tables[table_names[i]].columns.to_list()
+    col_names = column_names[table_names[i]]
+    column_lengths[table_names[i]] = max( [len(col_names[i]) for i in range(len(col_names))] )
+    column_lengths[table_names[i]] = max( column_lengths[table_names[i]], len(table_names[i]) )
+    
+################## Format display of columns
+
+max_length = max([ len(column_names[key]) for key in column_names ]) # Should be 115.
+padded_table_names = [ table_names[i]+" "*(column_lengths[table_names[i]] - len(table_names[i])) for i in range(len(table_names)) ]
+print("   ".join(padded_table_names))
+
+dividers = [ "="*column_lengths[table_names[i]] for i in range(len(table_names)) ]
+print("===".join(dividers))
+for i in range(max_length):
+    columns = [ " "*column_lengths[table_names[j]] for j in range(len(table_names)) ]
+    for j in range(len(table_names)):
+        if len(column_names[table_names[j]]) > i:
+            columns[j] = column_names[table_names[j]][i] + " "*(column_lengths[table_names[j]] - len(column_names[table_names[j]][i]) )
+    print(" | ".join(columns))
