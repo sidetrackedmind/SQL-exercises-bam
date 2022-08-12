071748496157710da2e2fdae1af5de30ec6bbe15
Michael Goff <michaelgoff@Michaels-MacBook-Pro.local>
2022-08-11 19:53:22 -0700

    README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# SQL
+# SQL Exercises
+
+This projects shows several small exercises to demonstrate skill with SQL. They are as follows.
+
+## Soccer
+
+Based on a [Kaggle data set](https://www.kaggle.com/code/dimarudov/data-analysis-using-sql/data) with information about matches, 
+players, and teams.
+
+###### get_db.py
+
+Displays all tables and all columns within the tables in the console.
+
+###### tests.py
+
+- Plot the portion of games that a team has won by year.
+- For a given player, find the unique set of teams and positions that the player has been on.
+- Plot the above data in a histogram.
--- Soccer/get_db.py
+++ Soccer/get_db.py
@@ -24,15 +24,17 @@ for i in range(len(table_names)):
     
 ################## Format display of columns
 
-max_length = max([ len(column_names[key]) for key in column_names ]) # Should be 115.
-padded_table_names = [ table_names[i]+" "*(column_lengths[table_names[i]] - len(table_names[i])) for i in range(len(table_names)) ]
-print("   ".join(padded_table_names))
+def display_columns():
+    max_length = max([ len(column_names[key]) for key in column_names ]) # Should be 115.
+    padded_table_names = [ table_names[i]+" "*(column_lengths[table_names[i]] - len(table_names[i])) for i in range(len(table_names)) ]
+    print("   ".join(padded_table_names))
 
-dividers = [ "="*column_lengths[table_names[i]] for i in range(len(table_names)) ]
-print("===".join(dividers))
-for i in range(max_length):
-    columns = [ " "*column_lengths[table_names[j]] for j in range(len(table_names)) ]
-    for j in range(len(table_names)):
-        if len(column_names[table_names[j]]) > i:
-            columns[j] = column_names[table_names[j]][i] + " "*(column_lengths[table_names[j]] - len(column_names[table_names[j]][i]) )
-    print(" | ".join(columns))
+    dividers = [ "="*column_lengths[table_names[i]] for i in range(len(table_names)) ]
+    print("===".join(dividers))
+    for i in range(max_length):
+        columns = [ " "*column_lengths[table_names[j]] for j in range(len(table_names)) ]
+        for j in range(len(table_names)):
+            if len(column_names[table_names[j]]) > i:
+                columns[j] = column_names[table_names[j]][i] + " "*(column_lengths[table_names[j]] - len(column_names[table_names[j]][i]) )
+        print(" | ".join(columns))
+display_columns()
